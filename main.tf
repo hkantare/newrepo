86735cd262dcc20b84facf731d19eb4db472127c
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-06-28 10:21:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ resource "ibm_is_vpc" "vpc" {
   tags = var.vpc_tags
 }
 
-resource "ibm_container_vpc_worker_pool" "default" {
+/*resource "ibm_container_vpc_worker_pool" "default" {
   count            = var.enable_default_pool_integration == "true" ? 1 : 0
 }
 
 variable "enable_default_pool_integration" {
   default = "true"
-}
+}*/
