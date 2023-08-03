b31a137a4fa5c4baaa7737efc5291b4e98385c5e
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2023-08-03 13:38:40 +0530

    Update main.tf to sleep for 900sec=15min

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@
 # This resource will destroy (potentially immediately) after null_resource.next
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_300_seconds" {
+resource "time_sleep" "wait_900_seconds" {
   depends_on = [null_resource.previous]
 
-  create_duration = "300s"
+  create_duration = "900s"
 }
 
-# This resource will create (at least) 300 seconds after null_resource.previous
+# This resource will create (at least) 900 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_300_seconds]
+  depends_on = [time_sleep.wait_900_seconds]
 }
 
 #data "ibm_is_vpc" "example" {
